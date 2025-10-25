3856d255b9179b9227d97f2c1db1ceb40020abc4
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-10-26 02:19:30 +0300

    move prisma files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TicketsModule } from './tickets/tickets.module';
 import { AuthModule } from './auth/auth.module';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   providers: [TicketsService, PrismaService],
